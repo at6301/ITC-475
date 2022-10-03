deec1649fbf8a5e084dcc07f48400f518e87826a
at6301 <at6301@live.missouristate.edu>
2022-10-03 17:28:55 -0500

    adding checkboxes

--- MegaTravelWebsite/activities.js
+++ MegaTravelWebsite/activities.js
@@ -1,7 +1,24 @@
 function showResults(selectedObject) {
+   let newZealDiv = document.getElementById("newZealDiv");
+   let maldivesDiv = document.getElementById("maldivesDiv");
+   let veniceDiv = document.getElementById("veniceDiv");
+   let cancunDiv = document.getElementById("cancunDiv");
 
-    document.getElementById("result");
-    
+   newZealDiv.style.visibility = 'hidden';
+   maldivesDiv.style.visibility = 'hidden';
+   veniceDiv.style.visibility = 'hidden';
+   cancunDiv.style.visibility = 'hidden';
 
-    
+   if(selectedObject == "New Zealand"){
+    newZealDiv.style.visibility = 'visible';
+   }
+   else if(selectedObject == "Maldives, South Asia"){
+    maldivesDiv.style.visibility = 'visible';
+   }
+   else if(selectedObject == "Venice, Italy"){
+    veniceDiv.style.visibility = 'visible';
+   }
+   else if(selectedObject == "Cancun"){
+    cancunDiv.style.visibility = 'visible';
+   }
 }
