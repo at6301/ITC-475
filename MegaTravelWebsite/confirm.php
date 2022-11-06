9ba372f28397e033fe9e952c46792fa326779c75
at6301 <112002109+at6301@users.noreply.github.com>
2022-11-06 10:35:21 -0600

    Add files via upload

--- MegaTravelWebsite/confirm.php
+++ MegaTravelWebsite/confirm.php
@@ -9,18 +9,32 @@
         <header>
             <img src="mega travel logo.png" alt="logo" style = "height: 140px;">
         </header>
-        <p>
-            Thank you for submitting your travel agent contact request! Here is the information you submitted:
-            Client Name: <?php echo $_POST["fname"] . $_POST["lname"];?>
-            Client Email: <?php echo $_POST["email"];?>
-            Client Phone Number: <?php echo $_POST["phoneNum"];?>
-            Number of Adults: <?php echo $_POST["adults"];?>
-            Number of Children: <?php echo $_POST["children"];?>
-            Destination: <?php echo $_POST["location"];?>
-            Travel Dates: <?php echo $_POST["dateStart"] . $_POST["dateEnd"];?>
-            Interested Activities:
-            An agent will be in touch with you soon!
-        </p>
+        <div id="returnInfoDiv" class="returnInfoDiv" style="padding: 10px;">
+            <p>
+                Thank you for submitting your travel agent contact request! Here is the information you submitted: <br>
+                <br>
+                Client Name: <?php echo $_POST["fname"] . " " . $_POST["lname"];?> <br>
+                Client Email: <?php echo $_POST["email"];?> <br>
+                Client Phone Number: <?php echo $_POST["phoneNum"];?> <br>
+                Number of Adults: <?php echo $_POST["adults"];?> <br>
+                Number of Children: <?php echo $_POST["children"];?> <br>
+                Destination: <?php echo $_POST["location"];?> <br>
+                Travel Dates: <?php echo $_POST["dateStart"] . " - " . $_POST["dateEnd"];?> <br>
+                Interested Activities: 
+                    <?php
+                       if (isset($_POST['submit'])){
+                            if(!empty($_POST['activity'])){
+                                foreach($_POST['activity'] as $selected){
+                                echo $selected . "</br>";
+                                }
+                            }
+                        } 
+                    ?>
+                <br>
+                <br>
+                An agent will be in touch with you soon! <br>
+            </p>
+        </div>
         <footer>
             <div style="margin-bottom: 20px;">Copyright Protected. All rights reserved.</div>
             <div>
